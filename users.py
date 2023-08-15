83e9d08af281bb21f1390d983c06bd36ab1e66e3
Jacob Willis <58826505+willisman31@users.noreply.github.com>
2023-08-14 21:37:22 -0700

    Update users.py

--- users.py
+++ users.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 class User:
 
@@ -12,18 +12,23 @@ class User:
         url = "https://www.reddit.com/user/" + self.name
         return url
 
-    def is_error(self):
-        response = requests.get(self.get_user_url())
+    def is_error(self, response):
         success_codes = [200, 201, 202]
         return response.status_code not in success_codes
 
     # returns first 25 submitted posts for user; not formatted in any way
     def get_posts(self):
         user_url = self.get_user_url() + "/submitted.json"
-        if self.is_error():
-            return False
-        posts = requests.get(user_url)
+        posts = requests.get(user_url, headers={'User-agent': 'Chrome/115.0.0.0'})
+        if self.is_error(posts):
+            return "Error"
         return posts.text
+    
+    # data is a raw response made up of a json object holding 1+ posts
+    def get_post_data(self, data):
+        json_data = json.loads(data)
+        for child in json_data['data']['children']:
+            print(child['data']['title'])
 
 # cakeday info is only sent on average for every 3 requests
     def attempt_get_cakeday(self):
